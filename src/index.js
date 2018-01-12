93e44d5c57a716faf7fc948ecd8a4ba5da4df570
Seth <itz.saga@gmail.com>
2018-01-12 15:29:04 -0600

    WIP webpack config

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () =>
+  <div>
+    <h1>Hello!</h1>
+  </div>;
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root') // eslint-disable-line
+);
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,23 @@ const Dotenv = require('dotenv-webpack');
  
 module.exports = {
   entry: './src/index.js',
-    output: {
-        path: __dirname + '/public',
-        filename: 'bundle.js'
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
   },
-  devtool: 'inline-source-map',
+  devtool: 'cheap-module-source-map',
   devServer: {
-    contentBase: '.'
+    contentBase: './public'
   },
   plugins: [
     new Dotenv({
-      path: './.env', // Path to .env file (this is the default) 
-      safe: true // load .env.example (defaults to "false" which does not use dotenv-safe) 
+      path: './.env'//, // Path to .env file (this is the default) 
+      //safe: true // load .env.example (defaults to "false" which does not use dotenv-safe) 
     })
-  ]
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ['babel-loader'] }
+    ]
+  }
 };
