e7f4b0abc1bcad993fd43ff3e7511f344fda4d28
Seth <itz.saga@gmail.com>
2018-01-12 14:47:04 -0600

    Change output path

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const Dotenv = require('dotenv-webpack');
 module.exports = {
   entry: './src/index.js',
     output: {
-        path: __dirname + '/dist',
+        path: __dirname + '/public',
         filename: 'bundle.js'
   },
   devtool: 'inline-source-map',
